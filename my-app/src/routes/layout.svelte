3cba4cb1ac6671522b91d405dd29778541f29892
Shanna van der Werve <shanna.van.der.werve@hva.nl>
2025-04-23 16:35:12 +0200

    Project setup: added routes

---
+++ my-app/src/routes/layout.svelte
@@ -0,0 +1,8 @@
+<a href="/">Home</a>
+<a href="/about-EFM">About EFM</a>
+<a href="/plan-your-visit">Plan your visit</a>
+<a href="/digital-catalog">Digital catalog</a>
+<a href="/research-institute">Research institute</a>
+<div>
+	<slot />
+</div>
