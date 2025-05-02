f5006567dd74ea88fcffe603feb6a14757081e94
Shanna van der Werve <shanna.van.der.werve@hva.nl>
2025-05-02 14:27:53 +0200

    feat: multiple results pages closes #15

--- src/routes/digital-catalog/+page.svelte
+++ src/routes/digital-catalog/+page.svelte
@@ -1,11 +1,48 @@
 <script>
+  	import { getBooks } from '$lib';
+
 	/** @type {import('./$types').PageData} */
-	export let data;
+	let { data } = $props();
+
+	let resultsPage = $state(data.resultsPage);
+	let books = $state(data.books);
+
+	async function nextResultsPage(event) {
+		event.preventDefault();
+		resultsPage++;
+		books = await getBooks(resultsPage);
+	}
+
+	async function previousResultsPage(event) {
+		event.preventDefault();
+		resultsPage--;
+		books = await getBooks(resultsPage);
+	}
 </script>
 <h1>Blog</h1>
+
+<noscript>
+	JAVASCRIPT DISABLED
+</noscript>
+
+<section id="results-nav">
+	<form action="/digital-catalog">
+		<!-- TODO: Add more hidden inputs containing the current filters sothat they are not removed when navigating -->
+		<input type="hidden" name="results-page" value={resultsPage}>
+		<input type="submit" name="results-page-action" value="previous" onclick={previousResultsPage}>
+		<input type="submit" name="results-page-action" value="next" onclick={nextResultsPage}>
+	</form>
+</section>
 <table>
+	<thead>
+		<tr>
+			<th>Titel</th>
+			<th>Auteur</th>
+			<th>Publicatie jaar</th>
+		</tr>
+	</thead>
 	<tbody>
-		{#each data.books as book}
+		{#each books as book}
 			<tr>
 				<td>{book.title}</td>
 				<td>{book.author}</td>
@@ -15,5 +52,23 @@
 	</tbody>
 	</table>
 <style>
-	
+	table, td, th {
+		border: none;
+		border-collapse: collapse;
+	}
+	thead, thead tr, th {
+		background-color: #ccc;
+	}
+
+	tbody tr:nth-of-type(even) td {
+		background-color: #eee;
+	}
+
+	th {
+		text-align: left;
+	}
+
+	td {
+		padding-right: 1em;
+	}
 </style>
