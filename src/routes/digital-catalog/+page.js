4997b14175a99c47cfe17f5a69b1e80ed4a8d82c
Shanna van der Werve <shanna.van.der.werve@hva.nl>
2025-05-08 23:53:03 +0200

    Revert "Merge branch '11-feature-place-of-publication-filter-ellenoor' into dev"
    This reverts commit d080171ad8d1339ef9d943de73690d5c029c367a, reversing
    changes made to c1a97b70ecdf756e874e77839e33a4b63d845c58.

--- src/routes/digital-catalog/+page.js
+++ src/routes/digital-catalog/+page.js
@@ -1,37 +1,21 @@
 /** @type {import('./$types').PageLoad} */
-import { PUBLIC_APIURL, PUBLIC_API_KEY } from '$env/static/public';
-import { arrayToObject, booksURL, placesURL } from '$lib';
+import { getBooks } from '$lib';
 
-export async function load({ fetch, url }) {
-	let query = booksURL;
+export async function load({ url, fetch }) {
 
-	const filterPlace = url.searchParams.get('publicationPlace');
-	if (filterPlace) {
-		query += `&fq[]=search_s_plaats_van_uitgave:"${filterPlace}"`; //Add searchterm to url
+	let resultsPage = parseInt(url.searchParams.get('results-page')) || 1;
+	const resultsPageAction = url.searchParams.get('results-page-action');
+	if (resultsPageAction === 'next') {
+		resultsPage++;
+	} else if (resultsPageAction === 'previous') {
+		resultsPage--
 	}
 
-	const res = await fetch(query);
-	const data = await res.json();
-	const books = data.media.map((book) =>{
-		const meta = arrayToObject(book.metadata, 'value')
-		return {
-			title: book.title,
-			author: meta.auteur,
-			publicationYear: meta.jaar,
-			publicationPlace: meta.plaats_van_uitgave
-		}
-	});
-
-	const resPlace = await fetch(placesURL);
-	const dataPublicationPlace = await resPlace.json();
-	const publicationPlaces = dataPublicationPlace.filter;
-
-	// Extract publicationPlaces filter data from dataPublicationPlace.filter safely
-	// const publicationPlaces = (dataPublicationPlace.filter ? dataPublicationPlace.filter.find(f => f.field === 'search_s_plaats_van_uitgave')?.filter : []) || [];
+	const { books, totalPages } = await getBooks(resultsPage, fetch);
 
 	return {
 		books,
-		publicationPlaces,
-		rawData: data.publicationPlace
+		resultsPage,
+		totalPages
 	}
 }
--- src/routes/digital-catalog/+page.svelte
+++ src/routes/digital-catalog/+page.svelte
@@ -1,5 +1,7 @@
 <script>
-	import FilterButtonUitgaveplaats from "$lib/filter-button-uitgaveplaats.svelte";
+  	import { getBooks } from '$lib';
+  	import PaginatedView from '$lib/paginated-view.svelte';
+
 	/** @type {import('./$types').PageData} */
 	let { data } = $props();
 
@@ -12,11 +14,14 @@
 		books = (await getBooks(resultsPage)).books;
 	})
 </script>
-
 <h1>Blog</h1>
 
-<FilterButtonUitgaveplaats publicationPlaces={data.publicationPlaces} />
+<noscript>
+	JAVASCRIPT DISABLED
+</noscript>
 
+<!-- bind: allows PaginatedView to update the value of resultsPage -->
+<PaginatedView bind:pageNr={resultsPage} name="results" totalPages={totalPages} />
 <table>
 	<thead>
 		<tr>
@@ -31,8 +36,28 @@
 				<td>{book.title}</td>
 				<td>{book.author}</td>
 				<td>{book.publicationYear}</td>
-				<td>{book.publicationPlace}</td>
 			</tr>
 		{/each}
 	</tbody>
-</table>
+	</table>
+<style>
+	table, td, th {
+		border: none;
+		border-collapse: collapse;
+	}
+	thead, thead tr, th {
+		background-color: #ccc;
+	}
+
+	tbody tr:nth-of-type(even) td {
+		background-color: #eee;
+	}
+
+	th {
+		text-align: left;
+	}
+
+	td {
+		padding-right: 1em;
+	}
+</style>
