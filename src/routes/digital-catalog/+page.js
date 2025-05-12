a9590b6d2b3f19f51852861d1623849788da77f2
EBok1 <144004887+EBok1@users.noreply.github.com>
2025-05-12 12:57:34 +0200

    Deleted old function

--- src/routes/digital-catalog/+page.js
+++ src/routes/digital-catalog/+page.js
@@ -12,28 +12,7 @@ export async function load({ url, fetch }) {
 		resultsPage--
 	}
 
-	const res = await fetch(query);
-	const data = await res.json();
-	const books = data.media.map((book) =>{
-		const meta = arrayToObject(book.metadata, 'value')
-		return {
-			title: book.title,
-			author: meta.auteur,
-			publicationYear: meta.jaar,
-			publicationPlace: meta.plaats_van_uitgave
-		}
-	});
 
-	const resPlace = await fetch(placesURL);
-	const dataPublicationPlace = await resPlace.json();
-	const publicationPlaces = dataPublicationPlace.filter;
 
-	// Extract publicationPlaces filter data from dataPublicationPlace.filter safely
-	// const publicationPlaces = (dataPublicationPlace.filter ? dataPublicationPlace.filter.find(f => f.field === 'search_s_plaats_van_uitgave')?.filter : []) || [];
 
-	return {
-		books,
-		publicationPlaces,
-		rawData: data.publicationPlace
-	}
 }
