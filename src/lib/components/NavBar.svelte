167a4e2ea9c6e9a2c5e30af6506bcbdafd33fe40
Alisa <alice.afanasieva@gmail.com>
2025-10-15 00:18:43 +0200

    feat(header): basis HTML-structuur toegevoegd voor navigatie menu
    - Header en navigatie  opgezet met semantische HTML
    - Logo, language, zoekveld en hamburger-menu toegevoegd
    - Eerste versie van menu-items (tickets, bezoekersinfo, collectie, etc.)
    
    Bestanden:
    - src/lib/components/NavBar.svelte
    
    refs: #104

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,66 @@
+<header class="header">
+  <div class="header__content">
+    <img
+      src="/logo/logo_white.png"
+      alt="Embassy of the Free Mind"
+      class="logo"
+    />
+    <nav class="nav">
+      <section class="nav-content">
+        <section class="right-top">
+          <div class="lang">EN ▼</div>
+          <div class="search">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="none"
+              stroke="white"
+              stroke-width="1"
+              viewBox="0 0 24 24"
+            >
+              <circle cx="11" cy="11" r="8" />
+              <line x1="21" y1="21" x2="16.65" y2="16.65" />
+            </svg>
+            <input type="text" placeholder="zoeken . . ." />
+          </div>
+          <div class="hamburger">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="28"
+              height="28"
+              viewBox="0 0 24 24"
+              stroke="white"
+              fill="none"
+              stroke-width="1"
+            >
+              <line x1="3" y1="6" x2="21" y2="6" />
+              <line x1="3" y1="12" x2="21" y2="12" />
+              <line x1="3" y1="18" x2="21" y2="18" />
+            </svg>
+          </div>
+        </section>
+        <ul class="nav__list">
+          <li class="nav__item">
+            <a href="#">tickets en evenementen</a>
+          </li>
+          <li class="nav__item">
+            <a href="#">bezoekersinformatie</a>
+          </li>
+          <li class="nav__item">
+            <a href="#">collectie</a>
+          </li>
+          <li class="nav__item">
+            <a href="#">online catalogus</a>
+          </li>
+          <li class="nav__item">
+            <a href="#">word member</a>
+          </li>
+          <li class="nav__item">
+            <a href="#">over ons</a>
+          </li>
+        </ul>
+      </section>
+    </nav>
+  </div>
+</header>
