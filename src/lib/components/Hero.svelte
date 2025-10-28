38d22bed55037e296b9ef2bed3252c77fd322a09
Alisa <alice.afanasieva@gmail.com>
2025-10-28 22:48:05 +0100

    feat(hero): add Hero component with HTML structure, base layout, load font and background
    refs #105

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,34 @@
+<section class="hero">
+	<div class="hero__content">
+		<h1>Vier 450 jaar</h1>
+    <h2>Amsterdam in het huis met de hoofden</h2>
+		<button class="hero__button">
+			Meer lezen
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="22"
+				height="22"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				viewBox="0 0 24 24"
+			>
+				<path d="M5 12h14M12 5l7 7-7 7" />
+			</svg>
+		</button>
+	</div>
+
+  <div class="hero__slides">
+    <img src="/images/background.png" alt="Background 1" class="active" />
+  </div>
+
+	<div class="hero__dots">
+		<button></button>
+		<button></button>
+		<button></button>
+	</div>
+</section>
+
+<style>
+
+</style>
