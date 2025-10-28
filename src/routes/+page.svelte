dfad980829876eca3cba46ed8394027f0b65c512
SebastiaanHag <sebastiaan.hagoort@hva.nl>
2025-10-28 12:55:07 +0100

    fix want dit ging fout tijdens de merge net

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
 <script>
 	import SectionWithLinks from '../lib/components/SectionWithLinks.svelte';
+	import Exhibitions from '$lib/components/Exhibitions.svelte';
 	const myImageUrl = '/images/image37.png';
 </script>
+
+<Exhibitions
+	imageUrl={myImageUrl}
+	title="Gewetensvrijheid!"
+	subtitle="Denken, geloven en onderzoeken in Europa, 1500-1800"
+	dates="28 april t/m 7 september 2025"
+	linkUrl="nog niets"
+/>
 
 <SectionWithLinks />
