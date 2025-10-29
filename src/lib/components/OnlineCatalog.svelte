cc6785a8b355420835d17509dbf93ba86c067a74
Recep <recep680@outlook.com>
2025-10-29 21:08:26 +0100

    #112 Catalog Prototype HTML

---
+++ src/lib/components/OnlineCatalog.svelte
@@ -0,0 +1,49 @@
+<section class="cat-container">
+    <section class="online-catalog">
+        <div class="cat-title">
+            <h2>Onze Collectie</h2>
+            <p>Een gedeelte van boeken uit onze collectie is al gedigitaliseerd. Met uw steun kunnen we de rest van onze collectie ook digitaliseren. Blader door objecten die het verhaal van onze geschiedenis vertellen. Elk object is voorzien van context, herkomst en datering.</p>
+        </div>
+        <a class="catalog-link" href="/">
+            Catalogus Bekijken
+            <span>
+                <img src="images/arrow-exhibition.svg" height="15" width="15" alt="arrow" />
+            </span>
+        </a>
+        <div class="card-container">
+            <div class="cards-wrapper">
+                <div class="card">
+                    <img class="book-cover" src="images/book-templ.png" alt="book-name">
+                    <p>Tiedemann, Dieterich</p>
+                    <h3>Versuch einer Erklärung des Ursprunges der Sprache</h3>
+                    <a href="#">
+                        <span>
+                            <img src="images/arrow-exhibition.svg" height="25" width="25" alt="arrow" />
+                        </span>
+                    </a>
+                </div>
+                <div class="card">
+                    <img class="book-cover" src="images/book-templ.png" alt="book-name">
+                    <p>Tiedemann, Dieterich</p>
+                    <h3>Versuch einer Erklärung des Ursprunges der Sprache</h3>
+                    <a href="#">
+                        <span>
+                            <img src="images/arrow-exhibition.svg" height="25" width="25" alt="arrow" />
+                        </span>
+                    </a>
+                </div>                
+                <div class="card">
+                    <img class="book-cover" src="images/book-templ.png" alt="book-name">
+                    <p>Tiedemann, Dieterich</p>
+                    <h3>Versuch einer Erklärung des Ursprunges der Sprache</h3>
+                    <a href="#">
+                        <span>
+                            <img src="images/arrow-exhibition.svg" height="25" width="25" alt="arrow" />
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+</section>
+
