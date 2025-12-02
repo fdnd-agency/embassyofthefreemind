53ed8c4fbedc12bea5f1511a11f3589131bdc44f
SebastiaanHag <sebastiaan.hagoort@hva.nl>
2025-12-02 11:37:29 +0100

    #190 threlte install

--- src/routes/exhibitions/+page.svelte
+++ src/routes/exhibitions/+page.svelte
@@ -1,4 +1,10 @@
 <script>
-    /** @type {import('./$types').PageProps} */
-    let { data } = $props();
+	/** @type {import('./$types').PageProps} */
+	let { data } = $props();
+	import { Canvas } from '@threlte/core';
+	import Scene from '../../lib/components/Scene.svelte';
 </script>
+
+<Canvas>
+	<Scene />
+</Canvas>
