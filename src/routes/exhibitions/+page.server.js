a34d2a47e1bbf753c1958b0fed5606d47a6a00a7
Recep <recep680@outlook.com>
2026-01-09 14:17:45 +0100

    test

--- src/routes/exhibitions/+page.server.js
+++ src/routes/exhibitions/+page.server.js
@@ -1,4 +1,6 @@
-/** @type {import('./$types').PageServerLoad} */
-export async function load() {
-    return {};
-};
+export async function load({ url }) {
+    const exhibitions = await fetch("https://fdnd-agency.directus.app/items/efm_exhibitions");
+    const exhibitionsData = await exhibitions.json();
+
+    return { exhibitions: exhibitionsData.data };
+}
